6146eb4cbe058dbfdacd1bf7a7930146f2788a3c
Anton Babakhin <anton-098+ya@yandex.ru>
2019-03-27 01:59:35 +0300

    Add manifest.json

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,8 @@
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const WorkboxPlugin = require('workbox-webpack-plugin');
+const WebpackPwaManifest = require('webpack-pwa-manifest');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
     entry: [
@@ -7,7 +10,36 @@ module.exports = {
         './src/index.js'
     ],
         
-    plugins: [
+    plugins: [        
+        new HtmlWebpackPlugin({
+            title: 'abvCSS - CSS-methgodology & framework',            
+            template: 'index-template.html',
+            filename: '../index.html',
+            minify: {
+                collapseWhitespace: true,
+                removeComments: true,
+                removeRedundantAttributes: true,
+                removeScriptTypeAttributes: true,
+                removeStyleLinkTypeAttributes: true,
+                useShortDoctype: true
+            },
+            hash: true            
+        }),
+        new WebpackPwaManifest({
+            name: 'abvCSS Methodology and Framework',
+            short_name: 'abvCSS Methodology and Framework',
+            description: 'abvCSS is a CSS-methodology and framework for creating modern, dynamic and supported web interfaces',
+            background_color: '#ffffff',
+            crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+            inject: false,
+            fingerprints: false,
+            icons: [
+              {
+                src: path.resolve('img/abvCSS-icon.png'),
+                sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+              }
+            ]
+        }),
         new MiniCssExtractPlugin({
             // Options similar to the same options in webpackOptions.output
             // both options are optional
